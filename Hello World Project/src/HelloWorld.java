23ee132f7b73b6646fdf974f329787976b7ccbe0
tarun.kumar <tarun.kumar@DASD-MXL351144P.em.ohio.gov>
2016-09-08 20:27:19 -0400

    added java runtime version code

--- Hello World Project/src/HelloWorld.java
+++ Hello World Project/src/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World, from tk");
+		System.out.println(System.getProperty("java.runtime.version"));
 	}
 
 }
